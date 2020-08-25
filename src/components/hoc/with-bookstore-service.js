6e0739a7945a3634e0a3bd1658b63294d364dd3d
i1-kmv <i1-kmv@mail.ru>
2020-08-25 13:20:31 +0200

    Создал необходимые для работы компоненты

---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {BookstoreServiceConsumer} from '../book-store-service-context'
+
+const withBookstoreService = () => (Wrapped) => {
+    return (props) => {
+        return (
+            <BookstoreServiceConsumer>
+                {
+                    (bookstoreService) => {
+                        return (
+                            <Wrapped {...props} bookstoreService = {bookstoreService}/>
+                        )
+                    }
+                }
+            </BookstoreServiceConsumer>
+        )
+    }
+}
+
+export default withBookstoreService
