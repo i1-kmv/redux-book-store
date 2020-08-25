37f6d02c22e2ecec64f2dade00ec1db33b2ad920
i1-kmv <i1-kmv@mail.ru>
2020-08-25 15:46:12 +0200

    сформировал структуру index.js

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,6 +1,13 @@
 
 export default class BookstoreService {
     getBooks() {
-        return []
+        return [
+            {id: 1,
+            title: 'Prodaction-Ready Microservices',
+            author: 'Susan J. Fowler'},
+            {id: 2,
+            title: 'Realise It',
+            author: 'Michael T. Ni'}
+        ]
     }
 }
