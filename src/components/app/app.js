064e33e69be0cc160b71d4f869d69f076854b6de
i1-kmv <i1-kmv@mail.ru>
2020-08-25 15:58:04 +0200

    Настроил роутинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,18 @@
 import React from "react"
 import './app.css'
-import withBookstoreService from "../hoc";
+import {Route, Switch} from "react-router-dom";
+import HomePage from "../pages/home-page";
+import CartPage from "../pages/card-page";
 
 
-const App = ({bookstoreService}) => {
-    console.log(bookstoreService.getBooks())
-    return <div>App</div>
+
+const App = () => {
+    return (
+        <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/cart" component={CartPage} />
+        </Switch>
+    )
 }
 
-export default withBookstoreService()(App)
+export default App
---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+import CartPage from "./card-page"
+import HomePage from "./home-page"
+export {
+    CartPage,
+    HomePage
+}
