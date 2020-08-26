27d5aaafe127d20f2c1a0d247dff490db1ab3c10
i1-kmv <i1-kmv@mail.ru>
2020-08-26 09:19:21 +0200

    Добавил обработчик ошибок и протестировал его работу

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,26 +3,37 @@ import BookListItem from "../book-list-item"
 import './book-list.css'
 import {connect} from 'react-redux'
 import withBookstoreService from "../hoc"
-import {booksLoaded, booksRequested} from "../../actions"
+import {booksLoaded, booksRequested, booksError} from "../../actions"
 import compose from "../../utils";
 import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 
 class BooksList extends Component {
 
     componentDidMount() {
-        const {bookstoreService, booksLoaded, booksRequested} = this.props
+        const {
+            bookstoreService,
+            booksLoaded,
+            booksRequested,
+            booksError} = this.props
+
         booksRequested()
         bookstoreService.getBooks()
             .then((data) => booksLoaded(data))
+            .catch((err) => booksError(err))
 
     }
 
     render() {
-        const {books, loading} = this.props
+        const {books, loading, error} = this.props
 
         if (loading) {
            return <Spinner/>
+        }
+
+        if (error) {
+            return <ErrorIndicator/>
         }
 
         return (
@@ -35,18 +46,18 @@ class BooksList extends Component {
                     })
                 }
             </ul>
-
         )
     }
 }
 
 const mapDispatchToProps =  {
     booksLoaded,
-    booksRequested
+    booksRequested,
+    booksError
 }
 
-const mapStateToProps = ({books, loading}) => {
-    return {books, loading}
+const mapStateToProps = ({books, loading, error}) => {
+    return {books, loading, error}
 }
 
 export default compose(
