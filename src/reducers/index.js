2f1b37ab5c2a58c235384fb5411ea27f80597bd0
i1-kmv <i1-kmv@mail.ru>
2020-08-29 20:05:54 +0200

    завершил рефакторинг редьюсера

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,14 +12,14 @@ const initialState = {
 };
 
 const updateOrder = (state, bookId, quantity) => {
-  const {books, cartItems} = state
+  const {bookList: {books}, shoppingCart: {cartItems}} = state
   const book = books.find(({id}) => id === bookId)
   const itemIndex = cartItems.findIndex(({id}) => id === bookId)
   const item = cartItems[itemIndex]
 
   const newItem = updateCartItem(book, item, quantity )
   return {
-    ...state,
+    orderTotal: 0,
     cartItems: updateCartItems(cartItems, newItem, itemIndex)
   }
 }
@@ -90,47 +90,39 @@ const updateBookList = (state, action) => {
   }
 
 const updateShoppingCart = (state, action) => {
+    switch (action.type) {
+      case 'BOOK_ADDED_TO_CART':
+        return updateOrder (state, action.payload, 1)
 
+      case 'BOOK_REMOVED_FROM_CART':
+        return updateOrder (state, action.payload, -1)
+      case 'ALL_BOOKS_REMOVED_FROM_CART':
+        const item = state.cartItems.find(({id}) => id === action.payload)
+        return updateOrder(state, action.payload, -item.count)
+    }
   }
 
 const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
-      return {
-        ...state,
-        books: [],
-        loading: true,
-        error: null
-      };
-
     case 'FETCH_BOOKS_SUCCESS':
-      return {
-        ...state,
-        books: action.payload,
-        loading: false,
-        error: null
-      };
-
     case 'FETCH_BOOKS_FAILURE':
       return {
         ...state,
-        books: [],
-        loading: false,
-        error: action.payload
-      };
-
+        bookList: updateBookList(state, action)
+      }
     case 'BOOK_ADDED_TO_CART':
-      return updateOrder (state, action.payload, 1)
-
     case 'BOOK_REMOVED_FROM_CART':
-      return updateOrder (state, action.payload, -1)
     case 'ALL_BOOKS_REMOVED_FROM_CART':
-      const item = state.cartItems.find(({id}) => id === action.payload)
-          return updateOrder(state, action.payload, -item.count)
+      return {
+        ...state,
+        shoppingCart: updateShoppingCart(state, action)
+      }
     default:
-      return state;
+      return state
   }
 }
+
 
 export default reducer;
