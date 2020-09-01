b6bdec9f10c7b390e6a22bb004f09cf1da73c2fa
i1-kmv <i1-kmv@mail.ru>
2020-09-01 07:47:23 +0200

    Разбил редьюсер на отдельные файлы

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
+import updateShoppingCart from './shopping-cart'
+import updateBookList from './book-list'
+
+
 
 const initialState = {
   bookList: {
@@ -11,118 +15,16 @@ const initialState = {
   }
 };
 
-const updateOrder = (state, bookId, quantity) => {
-  const {bookList: {books}, shoppingCart: {cartItems}} = state
-  const book = books.find(({id}) => id === bookId)
-  const itemIndex = cartItems.findIndex(({id}) => id === bookId)
-  const item = cartItems[itemIndex]
 
-  const newItem = updateCartItem(book, item, quantity )
+
+
+
+const reducer = (state , action) => {
   return {
-    orderTotal: 0,
-    cartItems: updateCartItems(cartItems, newItem, itemIndex)
-  }
-}
-
-const updateCartItems = (cartItems, item, idx) => {
-  if (item.count === 0) {
-    return  [
-      ...cartItems.slice(0, idx),
-      ...cartItems.slice(idx + 1)
-    ];
-  }
-  if (idx === -1) {
-    return [
-      ...cartItems,
-      item
-    ];
+    bookList: updateBookList(state, action),
+    shoppingCart: updateShoppingCart(state, action)
   }
 
-  return [
-    ...cartItems.slice(0, idx),
-    item,
-    ...cartItems.slice(idx + 1)
-  ];
-}
-
-const updateCartItem = (book, item = {}, quantity) => {
-
-  const {
-    id = book.id,
-    count = 0,
-    title = book.title,
-    total = 0 } = item;
-
-  return {
-    id,
-    title,
-    count: count + quantity,
-    total: total + quantity * book.price
-  };
-}
-
-const updateBookList = (state, action) => {
-
-  switch (action.type) {
-    case 'FETCH_BOOKS_REQUEST':
-      return {
-
-        books: [],
-        loading: true,
-        error: null
-      };
-
-    case 'FETCH_BOOKS_SUCCESS':
-      return {
-
-        books: action.payload,
-        loading: false,
-        error: null
-      };
-
-    case 'FETCH_BOOKS_FAILURE':
-      return {
-
-        books: [],
-        loading: false,
-        error: action.payload
-      };
-  }
-}
-
-const updateShoppingCart = (state, action) => {
-  switch (action.type) {
-    case 'BOOK_ADDED_TO_CART':
-      return updateOrder(state, action.payload, 1)
-
-    case 'BOOK_REMOVED_FROM_CART':
-      return updateOrder(state, action.payload, -1)
-    case 'ALL_BOOKS_REMOVED_FROM_CART':
-      const item = state.cartItems.find(({id}) => id === action.payload)
-      return updateOrder(state, action.payload, -item.count)
-  }
-}
-
-const reducer = (state = initialState, action) => {
-
-  switch (action.type) {
-    case 'FETCH_BOOKS_REQUEST':
-    case 'FETCH_BOOKS_SUCCESS':
-    case 'FETCH_BOOKS_FAILURE':
-      return {
-        ...state,
-        bookList: updateBookList(state, action)
-      }
-    case 'BOOK_ADDED_TO_CART':
-    case 'BOOK_REMOVED_FROM_CART':
-    case 'ALL_BOOKS_REMOVED_FROM_CART':
-      return {
-        ...state,
-        shoppingCart: updateShoppingCart(state, action)
-      }
-    default:
-      return state
-  }
 }
 
 
